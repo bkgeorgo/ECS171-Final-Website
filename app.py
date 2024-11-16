0913883e20ae6c65f23e205619487979ff6a0f95
Eddie <35238776+EddBr@users.noreply.github.com>
2024-11-15 18:26:27 -0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'mysecretkey'
+
+class InputForm(FlaskForm):
+    question1 = StringField("Answer this question: ")
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello!</p>"
+    form = InputForm()
+    return render_template("./form.html", form=form)
+    #return "<p>Hi!</p>"
 
